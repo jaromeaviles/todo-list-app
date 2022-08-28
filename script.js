6f03547a97621eb6ffd289f544d99fa15958da8a
jaromeaviles <jaromeaviles@gmail.com>
2022-08-29 01:12:44 +0800

    Done functionality fixed

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const clearTodo = document.querySelector('.clear');
 const todoLists = document.querySelector('#todos');
 
 let todos = [];
+let doneTodosArray = [];
 
 function showTodo() {
     for (let i = 0; i < todos.length; i++) {
@@ -22,6 +23,28 @@ function showTodo() {
         li.append(delBtn);
         li.append(doneBtn);
     }
+    if (doneTodosArray.length > 0) {
+        let doneLists = document.querySelectorAll('#todos li span');
+        let donebtns = document.querySelectorAll('#todos li .done');
+        let delbtns = document.querySelectorAll('#todos li .delete');
+        for (let j = 0; j < doneLists.length; j++) {
+            if (doneLists[doneTodosArray[j]] !== undefined) {
+                doneLists[doneTodosArray[j]].classList.add('strike');
+            }
+        }
+
+        for (let k = 0; k < donebtns.length; k++) {
+            if (donebtns[doneTodosArray[k]] !== undefined) {
+                donebtns[doneTodosArray[k]].classList.add('btn-disabled');
+            }
+        }
+
+        for (let l = 0; l < delbtns.length; l++) {
+            if (delbtns[doneTodosArray[l]] !== undefined) {
+                delbtns[doneTodosArray[l]].classList.add('btn-disabled');
+            }
+        }
+    }
     deleteTodo();
     doneTodo();
 }
@@ -38,6 +61,7 @@ addTodo.addEventListener('click', () => {
 clearTodo.addEventListener('click', () => {
     todoLists.replaceChildren();
     todos = [];
+    doneTodosArray = [];
     todoInput.value = '';
 });
 
@@ -45,8 +69,10 @@ clearTodo.addEventListener('click', () => {
 function deleteTodo() {
     let btns = document.querySelectorAll('.delete');
     for (let i = 0; i < btns.length; i++) {
-        btns[i].addEventListener('click', () => {
+        btns[i].addEventListener('click', e => {
         todos.splice(i, 1);
+        doneTodosArray.splice(i, 1);
+        e.target.classList.add('btn-disabled');
         todoLists.replaceChildren();
         showTodo();
         });
@@ -56,9 +82,13 @@ function deleteTodo() {
 function doneTodo () {
     let btns = document.querySelectorAll('.done');
     for (let i = 0; i < btns.length; i++) {
-        btns[i].addEventListener('click', () => {
+        btns[i].addEventListener('click', e => {
             let todo = document.querySelectorAll('#todos li span');
             todo[i].classList.add('strike');
+            doneTodosArray.push(i);
+            e.target.classList.add('btn-disabled');
+            todoLists.replaceChildren();
+            showTodo();
         });
     }
 }
